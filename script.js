71e931b0583f8bc8602bcc5be3c155020067c00e
Blake Fogel <114951798+Blake-Fogel@users.noreply.github.com>
2025-02-01 09:05:17 -0500

    Use clientWidth in resizePixels function

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 let pad = document.querySelector('.pad');
-for (let i = 0; i < 16; i++) {
+let pad_container = document.querySelector('.pad-container');
+let pixelCount = 16;
+const padObserver = new ResizeObserver(() => {
+    console.log("resized");
+    resizePad();
+    resizePixels();
+});
+document.querySelector('.clear-pad').addEventListener('click',() => {
     let padPixel = document.createElement('div');
     padPixel.classList.add('pad-pixel');
     pad.appendChild(padPixel);
+    resizePixels();
+});
+padObserver.observe(document.body);
+
+function resizePad() {
+}
+function resizePixels() {
+    document.querySelectorAll('.pad-pixel').forEach((element) => {
+        let desiredWidth = (pad.clientWidth / pixelCount) + 'px';
+        element.style.minWidth = desiredWidth;
+        console.log(element.style.minWidth);
+        console.log(desiredWidth);
+    });
+}
+function populatePixels() {
+    for (let i = 0; i < 18; i++) {
+        let padPixel = document.createElement('div');
+        padPixel.classList.add('pad-pixel');
+        pad.appendChild(padPixel);
+    }
+    resizePixels();
 }
