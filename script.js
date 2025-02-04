e0763b96a9954dcef6e11e66d86651db20302b78
Blake Fogel <114951798+Blake-Fogel@users.noreply.github.com>
2025-02-04 11:34:57 -0500

    resize works properly; worked out scrollbar appearing bug

--- script.js
+++ script.js
@@ -1,49 +1,45 @@
 let pad = document.querySelector('.pad');
-let pad_container = document.querySelector('.pad-container');
 let clear_pad = document.querySelector('.clear-pad');
-let pixelCount = 16;
+let pixelCount = 18;
 const padObserver = new ResizeObserver(() => {
-    console.log("resized");
-    resizePad();
-    resizePixels();
-});
-clear_pad.addEventListener('click',() => {
+        resizePad();
+        resizePixels();
+    });
+padObserver.observe(document.body);
+clear_pad.addEventListener('click', () => {
     let padPixel = document.createElement('div');
     padPixel.classList.add('pad-pixel');
     pad.appendChild(padPixel);
     resizePixels();
 });
-padObserver.observe(document.body);
-
+//populatePixels();
 function resizePad() {
     let clear_pad_measurements = window.getComputedStyle(clear_pad);
     let button_margin_height = parseFloat(clear_pad_measurements.marginTop) +
     parseFloat(clear_pad_measurements.marginBottom);
     let available_height = document.body.clientHeight - 
-    (button_margin_height + parseFloat(clear_pad_measurements.height));
-    console.log(available_height + "and width is " + document.body.clientWidth);
+    (button_margin_height + parseFloat(clear_pad_measurements.height)) - 0;
     if (available_height > document.body.clientWidth) {
-        let measurement = document.body.clientWidth + 'px';
+        let measurement = document.body.clientWidth - 1 + 'px';
         pad.style.width = measurement;
         pad.style.height = measurement;
-        console.log("set pad width to " + document.body.clientWidth);
-    } else {
+        console.log(measurement);
+     } else {
         let measurement = available_height + 'px';
         pad.style.height = measurement;
         pad.style.width = measurement;
-        console.log("Set pad height to " + available_height);
+        console.log(measurement);
     }
 }
 function resizePixels() {
     document.querySelectorAll('.pad-pixel').forEach((element) => {
-        let desiredWidth = (pad.clientWidth / pixelCount) + 'px';
-        element.style.minWidth = desiredWidth;
-        console.log(element.style.minWidth);
+        let desiredWidth = (pad.clientWidth / pixelCount);
+        element.style.width = desiredWidth + 'px';
         console.log(desiredWidth);
     });
 }
 function populatePixels() {
-    for (let i = 0; i < 18; i++) {
+    for (let i = 0; i < pixelCount**2; i++) {
         let padPixel = document.createElement('div');
         padPixel.classList.add('pad-pixel');
         pad.appendChild(padPixel);
